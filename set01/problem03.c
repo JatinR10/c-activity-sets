06562110af52385d8ab290e72dc669dfc50efe07
JatinRao10 <jatinr.bsc23@rvu.edu.in>
2024-01-29 12:24:32 +0530

    test

--- set01/problem03.c
+++ set01/problem03.c
@@ -4,9 +4,8 @@ int add(int a, int b);
 void output(int a, int b, int sum);
 int main()
 {
-    int a,b;
-    a= input();
-    b= input();
+    int a= input();
+    int b= input();
     int sum=0;
     sum=add(a,b);
     output(a,b,sum);
@@ -14,18 +13,18 @@ int main()
 int input()
 {
     int x;
-    printf("Enter the number :");
+    printf("Enter the number:\n");
     scanf("%d",&x);
     return x;
 }
+
 int add(int a, int b)
 {
-int sum=0;
-sum=a+b;
-return sum;
+    int sum=a+b;
+    return sum;
 }
 
 void output(int a, int b, int sum)
 {
-    printf("The sum is %d", sum);
+    printf("The sum is : %d \n", sum);
 }
